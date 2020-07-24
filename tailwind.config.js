f6bcffb7a274c34d43266482f3f438dd1859a4cc
Gustavo Borges <gustavo.mtborges@gmail.com>
2020-07-24 15:48:49 -0300

    Configuração inicial hugo e tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './content/**/*.html',
+    './layouts/**/*.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7FAE38',
+        secondary: '#006F9B'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}

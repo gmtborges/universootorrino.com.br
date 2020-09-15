40248d7e509761aa5332308622a0196807b1b30d
Gustavo Borges <gustavo.mtborges@gmail.com>
2020-09-14 21:19:12 -0300

    Adiciona sessões serviços, sobre nós

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#294860',
-        'primary-500': '#243C50',
-        secondary: '#E58E92'
+        primary: '#2F4858',
+        'primary-100': '#039590',
+        'primary-200': '#1C6E7D',
+        secondary: '#E58E92',
+        'secondary-300': '#C37D98',
+        'secondary-500': '#997096',
+        'secondary-700': '#6E6589',
+        'secondary-900': '#495873'
       }
     },
   },
